006e60b85120a2d8284e1b2850c43cdd1d891468
jaTockll12 <42842756+JacksonTolliday@users.noreply.github.com>
2019-05-15 12:41:15 -0400

    Updated from Brython Server: 5/15/2019 12:41:12 PM

--- Program.py
+++ Program.py
@@ -1,5 +1,11 @@
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame, TextAsset
 
-class Textrunner(App):
+def Textrunner(x):
     def __init__(self):
         super().__init__()
+        self.listenKeyEvent("keydown", "enter", self.nxt)
+        
+    def nxt:
+        for x in prompts:
+            print(x)
+            x = x+1
