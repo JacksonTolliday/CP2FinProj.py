e93a005d6db3fc41707cf2870ed1e6ab60cbe5c3
jaTockll12 <42842756+JacksonTolliday@users.noreply.github.com>
2019-05-21 10:13:27 -0400

    Updated from Brython Server: 5/21/2019 10:13:27 AM

--- Program.py
+++ Program.py
@@ -204,7 +204,7 @@ class Game(App):
             self.scorprint.destroy()
             self.scorprint = Score(self, (10,(Game.height/3)*2+5))
             self.listenKeyEvent("keydown", "t", self.throughgate)
-            self.listenKeyEvent("keydown", "l", self.moreinvest) #extend
+            self.listenKeyEvent("keydown", "i", self.moreinvest) #extend
             self.listenKeyEvent("keydown", "c", self.carreturn) #extend
         else:
             self.score = "You hold down the buzzer, leaning forward to what looks like a microphone, but before you can speak, the mic crackles loudly, interrupting you. The crackling continues for a few seconds, but then shuts off. The gate makes a weak whining noise, and it creaks open, just enough for you to push yourself through sideways. I mean it opened, but it did strike you as more than a little ominous... Press T to continue through the gate, press L to look around for another way in."
@@ -212,7 +212,7 @@ class Game(App):
             self.scorprint.destroy()
             self.scorprint = Score(self, (10,(Game.height/3)*2+5))
             self.listenKeyEvent("keydown", "t", self.throughgate)
-            self.listenKeyEvent("keydown", "l", self.moreinvest) #extend
+            self.listenKeyEvent("keydown", "i", self.moreinvest) #extend
 
     def throughgate(self, event):
         self.unlistenKeyEvent("keydown", "t", self.throughgate)
